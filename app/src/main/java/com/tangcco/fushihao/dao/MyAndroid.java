15616cb77560a5b96b990767e3837afb1585005a
15001260922 <2639654737@qq.com>
2017-02-04 14:59:49 +0800

    123

---
+++ app/src/main/java/com/tangcco/fushihao/dao/MyAndroid.java
@@ -0,0 +1,14 @@
+package com.tangcco.fushihao.dao;
+
+import android.util.Log;
+
+/**
+ * Created by dell on 2017/2/4.
+ */
+public class MyAndroid {
+
+    public void get(){
+        Log.e("tag","tag");
+    }
+
+}
--- app/src/main/java/com/tangcco/fushihao/dao/Xxxxxx.java
+++
@@ -1,9 +0,0 @@
-package com.tangcco.fushihao.dao;
-
-/**
- * Created by TT on 2017/1/23.
- */
-public class Xxxxxx {
-    private Integer id;
-
-}
